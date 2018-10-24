65c400f0915440a2202c2e4459fbb0f793ada0a7
Tjingjie <34300996+Tjingjie@users.noreply.github.com>
2018-10-24 11:13:02 +0800

    Add files via upload

--- com/poker/GameParser.java
+++ com/poker/GameParser.java
@@ -1,7 +1,12 @@
 package com.poker;
+/**
+ * 客户端消息解析类，使用静态方法对客户端的消息进行解析，产生相应的对象
+ */
 
 import bridge.domain.CallContract;
 import bridge.domain.Card;
+import bridge.domain.PlayerPosition;
+import bridge.domain.Rank;
 import bridge.domain.Suit;
 
 public class GameParser {
@@ -11,15 +16,29 @@ public class GameParser {
 		/**
 		 * 解析玩家发出的叫牌信息方法
 		 */
-		return new CallContract(0);
+		CallContract callContract;
+		PlayerPosition position=new PlayerPosition(message.charAt(0));
+		switch (message.charAt(2)) {
+		case 'P':
+			callContract=new CallContract(CallContract.PASS, position);
+			break;
+		case 'D':
+			callContract=new CallContract(CallContract.DOUBLE,position);
+			break;
+		case 'R':
+			callContract=new CallContract(CallContract.REDOUBLE,position);
+			break;
+		default:
+			callContract=new CallContract(message);
+			break;
+		}
+		return callContract;
 	}
 	
 	public static Card getCard(String message) {
 		/**
 		 * 获取该玩家出的牌的方法
 		 */
-		int rankScore='2';
-		Suit suit=new Suit('S');
-		return new Card(rankScore, suit);
+		return new Card(new Rank(message.charAt(1)), new Suit(message.charAt(0)));
 	}
 }
--- com/poker/PokerCommunicator.java
+++ com/poker/PokerCommunicator.java
@@ -5,14 +5,19 @@ package com.poker;
  */
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.websocket.RemoteEndpoint.Basic;
 import javax.websocket.Session;
+
+import com.google.gson.JsonArray;
 
 import bridge.domain.CallContract;
 import bridge.domain.Card;
 import bridge.domain.Deck;
 import bridge.domain.PlayerPosition;
+import bridge.domain.utils.BridgeHelper;
+
 
 public class PokerCommunicator {
 	private Basic sender;
@@ -27,29 +32,32 @@ public class PokerCommunicator {
 		/**
 		 * 将玩家手牌信息发给该玩家的方法
 		 */
+		sender.sendText(BridgeHelper.deckToPBNHand(deck));
 	}
 	
 	public void send(PlayerPosition position) throws IOException {
 		/**
 		 * 将当前行动玩家方信息发给玩家的方法
 		 */
+		sender.sendText(position.getFullName().substring(0, 1));
+	}
+	public void send(Card card) throws IOException{//向对应玩家发送其他玩家出的牌的方法
+		sender.sendText(card.getSuit().getShortName()+card.getRank().toString());
 	}
 	
+	public void send(CallContract contract)  throws IOException{
+		/**
+		 * 发送玩家叫品的方法
+		 */
+		sender.sendText(contract.getShortString());
+	}
+	
+
 	public void send(String message) throws IOException{
 		/**
 		 * 发送牌局信息以外的消息时使用的方法
 		 */
 		sender.sendText(message);
-	}
-	public void send(Card card) throws IOException{//向对应玩家发送其他玩家出的牌的方法
-		sender.sendText(card.getSuit().getShortName()+card.getRank().toString());
-	}
-	public void send(CallContract contract)  throws IOException{
-		/**
-		 * 发送玩家叫品的方法
-		 */
-		System.out.println(contract.toString());
-		//sender.sendText(contract);
 	}
 	
 }

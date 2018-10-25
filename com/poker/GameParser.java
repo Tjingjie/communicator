7351aaa8c13ecda4e266a87ab8574af2baf39ff9
Tjingjie <34300996+Tjingjie@users.noreply.github.com>
2018-10-25 16:31:04 +0800

    Add files via upload
    新增排序方法

--- com/poker/GameParser.java
+++ com/poker/GameParser.java
@@ -3,6 +3,11 @@ package com.poker;
  * 客户端消息解析类，使用静态方法对客户端的消息进行解析，产生相应的对象
  */
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import bridge.domain.CallContract;
 import bridge.domain.Card;
 import bridge.domain.PlayerPosition;
@@ -39,6 +44,32 @@ public class GameParser {
 		/**
 		 * 获取该玩家出的牌的方法
 		 */
-		return new Card(new Rank(message.charAt(1)), new Suit(message.charAt(0)));
+		String rank=message.substring(1, message.length());
+		int score=Integer.parseInt(rank);
+		return new Card(new Rank(message.charAt(1)), new Suit(score));
+	}
+	
+	public static List<Card>sort(List<Card> cards){
+		Collections.sort(cards, new Comparator<Card>() {
+			public int compare(Card a,Card b) {
+				int aOrder=a.getSuit().getOrder();
+				int bOrder=b.getSuit().getOrder();
+				int aScore=a.getRank().getScore();
+				int bScore=b.getRank().getScore();
+				if(aOrder>bOrder) {//比较花色
+					return 1;
+				}else if(aOrder<bOrder) {
+					return -1;
+				}else if(aScore<bScore){//比较点力
+					return 1;
+				}else if(aScore>bScore) {
+					return -1;
+				}else {
+					return 0;
+				}
+			}
+		});
+		
+		return cards;
 	}
 }
